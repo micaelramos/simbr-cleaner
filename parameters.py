ef4e0a190ddadb3821d4baa13f994ebbd04f6cdd
I822662 <micael.ramos@sap.com>
2021-03-03 19:01:41 -0300

    Argument parsing for input/output, basic testing

--- parameters.py
+++ parameters.py
@@ -8,26 +8,42 @@ fields_to_pop = ['TIPOBITO', 'IDADEMAE', 'ESCMAE', 'ESCMAE2010', 'SERIESCMAE', '
                      'PARTO', 'OBITOPARTO', 'PESO', 'EXAME', 'DTATESTADO', 'CAUSAMAT', 'ESCMAEAGR1', 'ESCFALAGR1',
                      'MORTEPARTO', 'DTCONCASO', 'ALTCAUSA']
 
-fields_to_rename = [{'old': 'DTOBITO', 'new': 'data_obito'}, {'old': 'HORAOBITO', 'new': 'hora_obito'},
-                        {'old': 'NATURAL_', 'new': 'naturalidade_pais'},
-                        {'old': 'CODMUNNATU', 'new': 'naturalidade_municipio'},
-                        {'old': 'DTNASC', 'new': 'data_nascimento'}, {'old': 'RACACOR', 'new': 'etnia'},
-                        {'old': 'ESTCIV', 'new': 'estado_cvil'}, {'old': 'ESC2010', 'new': 'escolaridade'},
-                        {'old': 'OCUP', 'new': 'profissao'}, {'old': 'CODMUNRES', 'new': 'municipio_residencia'},
-                        {'old': 'LOCOCOR', 'new': 'local_obito'}, {'old': 'CODMUNOCOR', 'new': 'municipio_obito'},
-                        {'old': 'ASSISTMED', 'new': 'houve_assistencia_medica'},
-                        {'old': 'CIRURGIA', 'new': 'houve_cirurgia'},
-                        {'old': 'NECROPSIA', 'new': 'confirmado_por_necropsia'},
-                        {'old': 'CAUSABAS', 'new': 'causa_basica'}, {'old': 'CIRCOBITO', 'new': 'circustancia_obito'},
-                        {'old': 'ACIDTRAB', 'new': 'acidente_dae_trabalho'},
-                        {'old': 'FONTE', 'new': 'fonte_informadora'},
-                        {'old': 'CAUSABAS_O', 'new': 'causa_basica_original'},
-                        {'old': 'TPPOS', 'new': 'foi_investigado'}, {'old': 'IDADE', 'new': 'idade'},
-                        {'old': 'SEXO', 'new': 'sexo'}]
+fields_to_format_date = ['DTOBITO', 'DTNASC']
 
-fields_to_format_date = ['data_obito', 'data_nascimento']
+fields_to_int = ['IDADE']
 
-fields_to_int = ['idade']
+fields_to_rename = [{'old': 'DTOBITO', 'new': 'death_date'}, {'old': 'HORAOBITO', 'new': 'death_time'},
+                        {'old': 'NATURAL_', 'new': 'country_of_origin'},
+                        {'old': 'CODMUNNATU', 'new': 'city_code'},
+                        {'old': 'DTNASC', 'new': 'birth_date'}, {'old': 'RACACOR', 'new': 'ethnicity'},
+                        {'old': 'ESTCIV', 'new': 'marital_status'}, {'old': 'ESC2010', 'new': 'education_level'},
+                        {'old': 'OCUP', 'new': 'occupation'}, {'old': 'CODMUNRES', 'new': 'city_of_residence'},
+                        {'old': 'LOCOCOR', 'new': 'place_of_death'}, {'old': 'CODMUNOCOR', 'new': 'city_of_death'},
+                        {'old': 'ASSISTMED', 'new': 'was_medically_assisted'},
+                        {'old': 'CIRURGIA', 'new': 'had_surgery'},
+                        {'old': 'NECROPSIA', 'new': 'is_confirmed_by_autopsy'},
+                        {'old': 'CAUSABAS', 'new': 'cause_of_death'}, {'old': 'CIRCOBITO', 'new': 'death_condition'},
+                        {'old': 'ACIDTRAB', 'new': 'was_work_related'},
+                        {'old': 'FONTE', 'new': 'info_source'},
+                        {'old': 'CAUSABAS_O', 'new': 'original_cause_of_death'},
+                        {'old': 'TPPOS', 'new': 'was_investigated'}, {'old': 'IDADE', 'new': 'age'},
+                        {'old': 'SEXO', 'new': 'gender'}]
+
+fields_to_rename_pt_br = [{'old': 'DTOBITO', 'new': 'data_obito'}, {'old': 'HORAOBITO', 'new': 'hora_obito'},
+                          {'old': 'CODMUNNATU', 'new': 'naturalidade_municipio'},
+                          {'old': 'DTNASC', 'new': 'data_nascimento'}, {'old': 'RACACOR', 'new': 'etnia'},
+                          {'old': 'ESTCIV', 'new': 'estado_cvil'}, {'old': 'ESC2010', 'new': 'escolaridade'},
+                          {'old': 'OCUP', 'new': 'profissao'}, {'old': 'CODMUNRES', 'new': 'municipio_residencia'},
+                          {'old': 'LOCOCOR', 'new': 'local_obito'}, {'old': 'CODMUNOCOR', 'new': 'municipio_obito'},
+                          {'old': 'ASSISTMED', 'new': 'houve_assistencia_medica'},
+                          {'old': 'CIRURGIA', 'new': 'houve_cirurgia'},
+                          {'old': 'NECROPSIA', 'new': 'confirmado_por_necropsia'},
+                          {'old': 'CAUSABAS', 'new': 'causa_basica'}, {'old': 'CIRCOBITO', 'new': 'circustancia_obito'},
+                          {'old': 'ACIDTRAB', 'new': 'acidente_dae_trabalho'},
+                          {'old': 'FONTE', 'new': 'fonte_informadora'},
+                          {'old': 'CAUSABAS_O', 'new': 'causa_basica_original'},
+                          {'old': 'TPPOS', 'new': 'foi_investigado'}, {'old': 'IDADE', 'new': 'idade'},
+                          {'old': 'SEXO', 'new': 'sexo'}]
 
 state_mappings = [{'source': '43', 'target': 'RS'}, {'source': '35', 'target': 'SP'}, {'source': '27', 'target': 'AL'},
                   {'source': '12', 'target': 'AC'}, {'source': '13', 'target': 'AM'}, {'source': '16', 'target': 'AP'},
--- sim_data_cleaner.py
+++ sim_data_cleaner.py
@@ -1,16 +1,17 @@
 import csv
 import json
+import argparse
 from parameters import fields_to_pop, fields_to_format_date, fields_to_int, fields_to_rename
 from datetime import datetime
 
 
-def clean_data(filename):
-    d = _csv_to_dict(filename)
+def clean_data(input_filename):
+    d = _csv_to_dict(input_filename)
     only_non_fetal = _filter_by_kv(d, "TIPOBITO", "2")
     clean_data = _pop_fields(only_non_fetal, fields_to_pop)
-    clean_data = _rename_fields(clean_data, fields_to_rename)
     clean_data = _change_to_int(clean_data, fields_to_int)
     clean_data = _format_date(clean_data, fields_to_format_date)
+    clean_data = _rename_fields(clean_data, fields_to_rename)
     return clean_data
 
 
@@ -56,11 +57,16 @@ def _format_date(data, fields):
     return data
 
 
-def _map(data, mapping):
-
+def save_results(filename, result_data):
+    with open(filename, 'w+') as file:
+        json.dump(result_data, file)
 
 
 if __name__ == '__main__':
-    data = clean_data('2019.csv')
-    with open('cleaned_2019.json', 'w') as file:
-        json.dump(data, file)
+    parser = argparse.ArgumentParser(description='SIM Data Cleaner.')
+    parser.add_argument('-i', dest='input', help='Input file path (CSV format)')
+    parser.add_argument('-o', dest='output', help='Output file path (JSON format)')
+    args = parser.parse_args()
+    data = clean_data(args.input)
+    save_results(args.output, data)
+    print('Successfully cleaned {} record(s)'.format(len(data)))
